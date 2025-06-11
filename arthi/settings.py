1d0e4f66d8d3f9193c100407c87b606378477e9d
ArthikaChinnusamy <arthisri0327@gmail.com>
2025-06-11 16:38:57 +0530

    database  add 8

--- arthi/settings.py
+++ arthi/settings.py
@@ -177,3 +177,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import dj_database_url
